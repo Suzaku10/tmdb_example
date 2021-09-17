b15b806a6bab47f2055123f973e70d606d3dceb9
yudhistira.yoga <yudhistiray198@gmail.com>
2021-09-18 01:08:22 +0700

    Add Skeleton Movies

--- lib/view/movies/movies_page.dart
+++ lib/view/movies/movies_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:tmdb_example/data/state.dart';
 import 'package:tmdb_example/store/movies_store/movies_store.dart';
+import 'package:tmdb_example/view/component/item_skeleton_movies.dart';
 import 'package:tmdb_example/view/movies/component/movies_segment_widget.dart';
 
 class MoviesPage extends StatefulWidget {
@@ -46,29 +48,16 @@ class _MoviesPageState extends State<MoviesPage> {
             return MoviesSegmentWidget(
                 title: "Top Rated", model: _store?.topRated?.results);
           } else {
-            return Container(
-              height: 200,
-              width: double.infinity,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
+            return _moviesSkeletonWidget();
           }
         });
-
       case 1:
         return Observer(builder: (_) {
           if (_store?.topRatedState == NetworkState.loaded) {
             return MoviesSegmentWidget(
                 title: "Upcoming", model: _store?.upcoming?.results);
           } else {
-            return Container(
-              height: 200,
-              width: double.infinity,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
+            return _moviesSkeletonWidget();
           }
         });
       case 2:
@@ -77,13 +66,7 @@ class _MoviesPageState extends State<MoviesPage> {
             return MoviesSegmentWidget(
                 title: "Popular", model: _store?.popular?.results);
           } else {
-            return Container(
-              height: 200,
-              width: double.infinity,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
+            return _moviesSkeletonWidget();
           }
         });
       case 3:
@@ -92,17 +75,41 @@ class _MoviesPageState extends State<MoviesPage> {
             return MoviesSegmentWidget(
                 title: "Now Playing", model: _store?.nowPlaying?.results);
           } else {
-            return Container(
-              height: 200,
-              width: double.infinity,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
+            return _moviesSkeletonWidget();
           }
         });
       default:
         return Container();
     }
+  }
+
+  Widget _moviesSkeletonWidget() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8).copyWith(top: 16),
+          child: Shimmer.fromColors(
+              child: Container(
+                color: Colors.white,
+                height: 10,
+                width: 70,
+              ),
+              baseColor: Colors.grey,
+              highlightColor: Colors.amber),
+        ),
+        Flexible(
+          child: Container(
+            height: 440,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => ItemSkeletonMoviesWidget(),
+              itemCount: 5,
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
